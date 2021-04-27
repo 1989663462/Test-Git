94015aabfcdc939d30771eabbf9fce69db994f02
wwb <wwb@11.com>
2021-04-27 14:12:14 +0800

    111

--- src/cn/test/Demo.java
+++ src/cn/test/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("git hello");
 		System.out.println("add ...");
+		System.out.println("new 2.....");
 		System.out.println("new 1.....");
 	}
 }
